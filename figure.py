aaac3d4bce3c7c38f9708140a9108449dd7432b0
migle <migle.luko94@gmail.com>
2024-04-06 18:07:54 +0300

    Amended the class Figure() to get plot settings from FigureSettings() instance.

--- figure.py
+++ figure.py
@@ -1,25 +1,32 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
 from constants import AU
 from decorators import status_update
 
 
 class Figure:
-    def __init__(self, particle_list):
-        self.figure = plt.figure(figsize=(5, 5))
+    def __init__(self, particle_list, figure_settings):
+        self.figure = plt.figure(figsize=figure_settings.fig_size)
         self.scatter = None
+        self.lines = []
         self.position_list = particle_list.position_list
         self.mass_list = particle_list.mass_list
+
         self._xlim = [-2 * AU, 2 * AU]
         self._ylim = [-2 * AU, 2 * AU]
-        self.background = 'black'
-        self.particle_colour = 'white'
-        self.edge_colour = 'black'
-        self.plot_colour = 'black'
-        self.line_weight = 1
-        self.lines = []  # Added lines attribute to hold trajectory lines
-        self.colours = ['white', 'white', 'white']
-        self.sizes = [1e2, 1e2, 1e2]
+
+        self.background = figure_settings.background
+        self.edge_colour = figure_settings.edge_colour
+        self.plot_colour = figure_settings.plot_colour
+        self.particle_colour = figure_settings.particle_colour
+        self.custom_colours = figure_settings.custom_colours
+        self.size = np.log10(self.mass_list)
+        self.sizes = figure_settings.custom_sizes
+        self.line_weight = figure_settings.line_weight
+        self.line_colour = figure_settings.line_colour
+        self.line_style = figure_settings.line_style
+        self.line_custom_colours = figure_settings.line_custom_colours
 
     @status_update
     def plot_figure(self):
@@ -28,13 +35,9 @@ class Figure:
         axes = plt.axes(xlim=self._xlim, ylim=self._ylim)
         axes.set_aspect(1)
         axes.set_facecolor(self.plot_colour)
-        self.scatter = axes.scatter(self.position_list[:, 0], self.position_list[:, 1], s=self.sizes, c=self.colours,
+        self.scatter = axes.scatter(self.position_list[:, 0], self.position_list[:, 1], s=self.size, color=self.particle_colour,
                                     edgecolors=self.edge_colour, lw=self.line_weight)
 
         for pos in self.position_list:
-            line, = axes.plot(pos[0], pos[1], color='white', linestyle='-', linewidth=1)
-            self.lines.append(line)
-
-        for pos, color, size in zip(self.position_list, self.colours, self.sizes):
-            line, = axes.plot(pos[0], pos[1], color=color, linestyle='-', linewidth=size / 10)
+            line, = axes.plot(pos[0], pos[1], color=self.line_colour, linestyle=self.line_style, linewidth=self.line_weight)
             self.lines.append(line)
