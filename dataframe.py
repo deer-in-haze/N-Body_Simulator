591d1a3e40f429c89321500623251bdf8ec8f85e
migle <migle.luko94@gmail.com>
2024-04-07 21:23:43 +0300

    Added  missing value checks to the clean_method. Added a missing values flag attribute to the Data() class.

--- dataframe.py
+++ dataframe.py
@@ -1,5 +1,6 @@
 import pandas as pd
-from calculations import OrbitalVelocity
+from constants import DAY, MASS_SUN, MASS_EARTH, AU
+from decorators import status_update
 
 
 class Data:
@@ -8,33 +9,31 @@ class Data:
         self.df = None
         self.column_names = []
         self.planet_system = None
+        self.has_missing_values = False
 
+    @status_update
     def read_data(self):
         self.df = pd.read_csv(self.path, skiprows=16)
         self.df['rowupdate'] = pd.to_datetime(self.df['rowupdate'])  # changing to datetime format
-        print(self.df)
 
+    @status_update
     def clean_data(self):
         most_recent_data_index = self.df.groupby('pl_name')['rowupdate'].idxmax()
         self.df = self.df.loc[most_recent_data_index]
-        print(self.df)
+        has_missing_values = self.df.isna().any().any()
+        print(f'Dataframe has missing values: {has_missing_values}')
+        rows_with_missing_data = self.df[self.df.isna().any(axis=1)]
+        planets_with_missing_data = rows_with_missing_data['pl_name']
+        print(f'Planets with incomplete data: {len(planets_with_missing_data)}')
 
     def group_data(self):
         grouped_data = self.df.groupby('hostname')
-        print(grouped_data)
         return grouped_data
 
     def get_group(self, hostname):
         self.planet_system = self.group_data().get_group(hostname)
+        self.has_missing_values = self.planet_system.isna().any().any()
         return self.planet_system
-
-
-data = Data('/home/migle/PycharmProjects/n_body_simulator/nasa_exoplanet_data.csv')
-data.read_data()
-data.clean_data()
-data.group_data()
-
-system = data.get_group('ups And')  # veliau pakeisiu
 
 
 class PlanetSystemData:
@@ -43,34 +42,23 @@ class PlanetSystemData:
         self.host_star = self.planet_system.iloc[0, 1]
         self.host_mass = self.planet_system.iloc[0, 7]
         self.planet_count = self.planet_system.iloc[0, 3]
-        self.planet_list = []
+        self.planet_name_list = []
         self.planet_mass_list = []
         self.semi_major_axis_list = []
+        self.position_list = []
         self.orbital_period_list = []
         self.orbital_velocity_list = []
         self.particle_list = []
         self.system = {}
 
+    @status_update
     def get_planet_data(self):
         for planet_name, planet_mass, a, T in zip(self.planet_system.loc[:, 'pl_name'],
                                                   self.planet_system.loc[:, 'pl_bmassj'],
                                                   self.planet_system.loc[:, 'pl_orbsmax'],
                                                   self.planet_system.loc[:, 'pl_orbper']):
-            self.planet_list.append(planet_name)
-            self.planet_mass_list.append(planet_mass)
-            self.semi_major_axis_list.append(a)
-            self.orbital_period_list.append(T)
-
-        print(self.planet_count)
-        print(self.host_star)
-        print(self.host_mass)
-        print(self.planet_list)
-        print(self.semi_major_axis_list)
-        print(self.orbital_period_list)
-        print(self.planet_mass_list)
-
-
-planet_system_data = PlanetSystemData(system)
-planet_system_data.get_planet_data()
-calc = OrbitalVelocity(planet_system_data)
-calc.calculate_orbital_velocity()
+            self.planet_name_list.append(planet_name)
+            self.planet_mass_list.append(planet_mass * MASS_EARTH)
+            self.semi_major_axis_list.append(a * AU)
+            self.position_list.append([a * AU, 0])
+            self.orbital_period_list.append(T * DAY)
