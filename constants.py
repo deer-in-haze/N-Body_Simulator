62858f7774931dd065d3dbf323ce33bdc0742f22
migle <migle.luko94@gmail.com>
2024-04-10 21:26:26 +0300

    Added NASA EXOPLANET DATABASE API key, data destination folder, filenames.

--- constants.py
+++ constants.py
@@ -17,3 +17,13 @@ SMALL_MASS_TIMESTEP = 20 / 1000
 
 """MISCELLANEOUS"""
 OFFSET = 0
+
+"""APIs"""
+NASA_DATABASE_API_KEY = ("https://exoplanetarchive.ipac.caltech.edu/TAP/sync?query=select+pl_name,hostname,sy_snum,"
+                         "sy_pnum,pl_orbper,pl_orbsmax,pl_bmassj,st_mass,rowupdate+from+ps&format=csv")
+"""PATHS"""
+DATA_FOLDER = '/home/migle/PycharmProjects/n_body_simulator/DATA/'
+
+"""FILENAMES"""
+ORIGINAL_DATA_FILENAME = 'original_data.csv'
+CLEAN_DATA_FILENAME = 'clean_data.csv'
