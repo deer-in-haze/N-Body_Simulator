d9c3bcf935d61190358e47303bd0ed3e1285336a
migle <migle.luko94@gmail.com>
2024-04-11 12:53:34 +0300

    Fixed singleton decorator to suit different classes.

--- decorators.py
+++ decorators.py
@@ -16,9 +16,9 @@ def status_update(func):
 def singleton(cls):
     instances = {}
 
-    def get_instance(api_url):
+    def get_instance(*args, **kwargs):
         if cls not in instances:
-            instances[cls] = cls(api_url)
+            instances[cls] = cls(*args, **kwargs)
         return instances[cls]
 
     return get_instance
