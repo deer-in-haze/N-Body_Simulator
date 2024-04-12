c73f191e80cc96b11587d6df17f67c8f22a47fbf
migle <migle.luko94@gmail.com>
2024-04-12 13:47:21 +0300

    Fixed set_step_size, so it would also set half_step_size.

--- settings.py
+++ settings.py
@@ -1,11 +1,11 @@
-from constants import GRAVITY_CONST, DAY, HOUR
+from constants import GRAVITY_CONST, DAY, HOUR, SMALL_MASS_TIMESTEP
 from decorators import singleton
 
 @singleton
 class Settings:
     def __init__(self, **kwargs):
         self._gravity_const = kwargs.get('gravity_const', GRAVITY_CONST)
-        self._step_size = kwargs.get('step_size', DAY)
+        self._step_size = kwargs.get('step_size', SMALL_MASS_TIMESTEP)
         self._softening = kwargs.get('softening', 0.1)
         self._half_step_size = 0.5 * self._step_size
 
@@ -17,6 +17,7 @@ class Settings:
 
     def set_step_size(self, step_size):
         self._step_size = step_size
+        self._half_step_size = 0.5 * step_size
 
     def get_step_size(self):
         return self._step_size
