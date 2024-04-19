04c88ccc8f8243cb8b0654a7c0149b109710903b
migle <migle.luko94@gmail.com>
2024-04-19 22:38:04 +0300

    Adjusted class Simulation to incorporate figure settings and animation settings builders. Encapsulated the class.

--- simulation.py
+++ simulation.py
@@ -1,37 +1,50 @@
 from particles import Particles
 from animation import ParticleAnimation
-from calculations import CentreOfMass, Acceleration
-from settings import Settings
+from calculations import CentreOfMass, Acceleration, Limits
 from figure import Figure
-from figure_settings import FigureSettings
+from figure_settings import FigureSettingsBuilder
 from decorators import status_update
+from animation_settings import AnimationSettingsBuilder
 
 
 class Simulation:
-    def __init__(self, particle_system, settings, frames, filename):
-        self.filename = filename
-        self.settings = settings
-        self.frames = frames
-        self.particles = Particles()
-        self.particle_system = particle_system
+    def __init__(self, particle_system, settings, figure_settings, frames, filename):
+        self.__filename = filename
+        self.__settings = settings
+        self.__figure_settings = figure_settings
+        self.__frames = frames
+        self.__particles = Particles()
+        self.__particle_system = particle_system
+
+    def __initiate_particles(self):
+        for particle in self.__particle_system:
+            self.__particles.add_particle(particle)
 
     @status_update
     def start(self):
-        for particle in self.particle_system:
-            self.particles.add_particle(particle)
 
-        com = CentreOfMass(self.particles.mass_list, self.particles.position_list, self.particles.velocity_list)
+        self.__initiate_particles()
+
+        com = CentreOfMass(self.__particles)
         com.centre_positions_and_velocities()
 
-        fig_settings = FigureSettings(particle_list=self.particles)
-        fig_settings.calculate_limits()
-        fig = Figure(self.particles, fig_settings)
-        fig.plot_figure()
+        limits = Limits(self.__particles)
+        limits.calculate_limits()
 
-        acceleration = Acceleration(self.particles.position_list, self.particles.mass_list,
-                                    self.settings.get_softening(), self.settings.get_gravity_const())
+       # figure_settings_builder = FigureSettingsBuilder()
+       # figure_settings = figure_settings_builder.set_line_style(':').build()
 
-        animation_func = ParticleAnimation(fig, acceleration, self.particles, self.settings, lines_on=True,
-                                           frames=self.frames)
+        figure = Figure(self.__particles, limits, self.__figure_settings)
+        figure.plot_figure()
+
+        acceleration = Acceleration(self.__particles, self.__settings)
+
+        animation_settings_builder = AnimationSettingsBuilder()
+        animation_settings = animation_settings_builder.build()
+
+        animation_func = ParticleAnimation(figure, acceleration,
+                                           self.__particles,
+                                           self.__settings,
+                                           animation_settings)
         animation_func.create_animation()
-        animation_func.save_animation(filename=self.filename)
+        animation_func.save_animation(filename=self.__filename)
