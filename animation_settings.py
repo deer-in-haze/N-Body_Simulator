2edca994c5d76d354d7086f89fa3521b0dc9a739
migle <migle.luko94@gmail.com>
2024-04-20 17:10:48 +0300

    Adjusted coding style to match SimulationSettings.

--- animation_settings.py
+++ animation_settings.py
@@ -1,71 +1,53 @@
-class AnimationSettings:
-    def __init__(self):
-        self.__lines = True
-        self.__frames = 200
-        self.__interval = 20
-        self.__blit = False
+from settings import Settings
 
-    def set_lines(self, boolean):
-        self.__lines = boolean
+
+class AnimationSettings(Settings):
+    class Builder():
+        def __init__(self):
+            self.__lines = True
+            self.__frames = 200
+            self.__interval = 20
+            self.__blit = False
+
+        def set_lines(self, boolean):
+            self.__lines = boolean
+            return self
+
+        def set_frames(self, frames):
+            self.__frames = frames
+            return self
+
+        def set_interval(self, interval):
+            self.__interval = interval
+            return self
+
+        def set_blit(self, boolean):
+            self.__blit = boolean
+            return self
+
+        def build(self):
+            return AnimationSettings(self.__lines,
+                                     self.__frames,
+                                     self.__interval,
+                                     self.__blit)
+
+    def __init__(self, lines, frames, interval, blit):
+        self.__lines = lines
+        self.__frames = frames
+        self.__interval = interval
+        self.__blit = blit
 
     def get_lines(self):
         return self.__lines
 
-    def set_frames(self, frames):
-        self.__frames = frames
-
     def get_frames(self):
         return self.__frames
-
-    def set_interval(self, interval):
-        self.__interval = interval
 
     def get_interval(self):
         return self.__interval
 
-    def set_blit(self, boolean):
-        self.__blit = boolean
-
     def get_blit(self):
         return self.__blit
 
-
-class AnimationSettingsBuilder:
-    def __init__(self):
-        self.__settings = None
-        self.__reset()
-
-    def __reset(self):
-        self.__settings = AnimationSettings()
-        return self
-
-    def set_lines(self, boolean):
-        self.__settings.set_lines(boolean)
-        return self
-
-    def get_lines(self):
-        return self.__settings.get_lines()
-
-    def set_frames(self, frames):
-        self.__settings.set_interval(frames)
-        return self
-
-    def get_frames(self):
-        return self.__settings.get_frames()
-
-    def set_interval(self, interval):
-        self.__settings.set_interval(interval)
-        return self
-
-    def get_interval(self):
-        return self.__settings.get_interval()
-
-    def set_blit(self, boolean):
-        self.__settings.set_blit(boolean)
-        return self
-
-    def get_blit(self):
-        return self.__settings.get_blit()
-
-    def build(self):
-        return self.__settings
+    def display(self):
+        pass
