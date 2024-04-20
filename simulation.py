000b91b972db4fb3f800a6d7502343f7643ed7ae
migle <migle.luko94@gmail.com>
2024-04-20 17:14:19 +0300

    Adjusted the code slightly to match changes in other files.

--- simulation.py
+++ simulation.py
@@ -2,19 +2,21 @@ from particles import Particles
 from animation import ParticleAnimation
 from calculations import CentreOfMass, Acceleration, Limits
 from figure import Figure
-from figure_settings import FigureSettingsBuilder
+from figure_settings import FigureSettings
 from decorators import status_update
-from animation_settings import AnimationSettingsBuilder
+from animation_settings import AnimationSettings
+from simulation_settings import SimulationSettings
 
 
 class Simulation:
-    def __init__(self, particle_system, settings, figure_settings, frames, filename):
-        self.__filename = filename
-        self.__settings = settings
-        self.__figure_settings = figure_settings
-        self.__frames = frames
-        self.__particles = Particles()
+    def __init__(self, particle_system, step_size, gravity_const, frames, filename):
         self.__particle_system = particle_system
+        self.__step_size = step_size
+        self.__gravity_const = gravity_const
+        self.__frames = frames
+        self.__filename = filename
+        self.__particles = Particles()
+
 
     def __initiate_particles(self):
         for particle in self.__particle_system:
@@ -24,6 +26,7 @@ class Simulation:
     def start(self):
 
         self.__initiate_particles()
+        print(self.__frames)
 
         com = CentreOfMass(self.__particles)
         com.centre_positions_and_velocities()
@@ -31,20 +34,20 @@ class Simulation:
         limits = Limits(self.__particles)
         limits.calculate_limits()
 
-       # figure_settings_builder = FigureSettingsBuilder()
-       # figure_settings = figure_settings_builder.set_line_style(':').build()
+        figure_settings = FigureSettings.Builder().set_line_style(':').build()
 
-        figure = Figure(self.__particles, limits, self.__figure_settings)
+        figure = Figure(self.__particles, limits, figure_settings)
         figure.plot_figure()
 
-        acceleration = Acceleration(self.__particles, self.__settings)
+        simulation_settings = SimulationSettings.Builder().set_step_size(self.__step_size).set_gravity_const(self.__gravity_const).build()
 
-        animation_settings_builder = AnimationSettingsBuilder()
-        animation_settings = animation_settings_builder.build()
+        acceleration = Acceleration(self.__particles, simulation_settings)
+
+        animation_settings = AnimationSettings.Builder().set_frames(self.__frames).build()
 
         animation_func = ParticleAnimation(figure, acceleration,
                                            self.__particles,
-                                           self.__settings,
+                                           simulation_settings,
                                            animation_settings)
         animation_func.create_animation()
         animation_func.save_animation(filename=self.__filename)
